a1d815a0fee59cd5b1ea8f9488fc37a51e74376d
sange5 <163669819+sange5@users.noreply.github.com>
2024-11-29 16:34:31 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,45 @@
-ECHO is on.
+-- INNER JOIN to retrieve all bookings and respective users, ordered by booking date
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.booking_date,
+    users.user_id,
+    users.username
+FROM 
+    bookings
+INNER JOIN 
+    users
+ON 
+    bookings.user_id = users.user_id
+ORDER BY 
+    bookings.booking_date ASC;
+
+-- LEFT JOIN to retrieve all properties and their reviews, including properties with no reviews, ordered by property name
+SELECT 
+    properties.property_id,
+    properties.property_name,
+    reviews.review_id,
+    reviews.review_text
+FROM 
+    properties
+LEFT JOIN 
+    reviews
+ON 
+    properties.property_id = reviews.property_id
+ORDER BY 
+    properties.property_name ASC;
+
+-- FULL OUTER JOIN to retrieve all users and all bookings, ordered by username
+SELECT 
+    users.user_id,
+    users.username,
+    bookings.booking_id,
+    bookings.property_id
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings
+ON 
+    users.user_id = bookings.user_id
+ORDER BY 
+    users.username ASC;
